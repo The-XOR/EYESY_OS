f9b6943c213b7d0c50fc80d32997a49d8db5ea03
The XOR <1h3x0r@gmail.com>
2021-07-25 11:16:36 +0200

    vedemo ora

---
+++ config/openFrameworks/addons/ofxLua/scripts/generate_bindings.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# generate Lua bindings for all platforms
+#
+
+# stop on error
+set -e
+
+SWIG_FLAGS="DEST_DIR=../src/bindings"
+
+###
+
+cd $(dirname $0)/../swig
+
+make desktop $SWIG_FLAGS $@
+make ios $SWIG_FLAGS $@
+make linuxarm $SWIG_FLAGS $@
+make header $SWIG_FLAGS $@
+make libs $SWIG_FLAGS $@
